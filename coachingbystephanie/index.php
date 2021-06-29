0db08088f24055983e15ec2f81f59b58284affb9
Nathan Fitzgerald <nathan@nathanfitzgerald.com>
2021-06-29 15:52:05 -0700

    test CF webhook

--- coachingbystephanie/index.php
+++ coachingbystephanie/index.php
@@ -1,5 +1,12 @@
 <?php
 
+header('Content-Type as application/json');
+date_default_timezone_set('UTC');
+echo '{ "time": "' . date('Y-m-d H:m:s') . 'UTC" }';
+exit;
+
+
+
 define("ACTIVECAMPAIGN_URL", "https://coachingbystephanie.api-us1.com");
 define("ACTIVECAMPAIGN_API_KEY", "fced1b3f5c04f685abbf06da03622f73ddcbd97a1b8ee38bf6905fa37fd67c5c909119e6");
 require_once(__DIR__.'/../activecampaign-api-php/includes/ActiveCampaign.class.php');
