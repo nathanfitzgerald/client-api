19a8ed777b8ae14331dea029c530a0f18190ed9a
Nathan Fitzgerald <nathan@nathanfitzgerald.com>
2021-06-29 15:17:54 -0700

    syncing a contact

--- coachingbystephanie/index.php
+++ coachingbystephanie/index.php
@@ -8,17 +8,22 @@ $ac = new ActiveCampaign(ACTIVECAMPAIGN_URL, ACTIVECAMPAIGN_API_KEY);
 // Adjust the default cURL timeout
 $ac->set_curl_timeout(10);
 
-if (!(int)$ac->credentials_test()) {
-	echo "<p>Access denied: Invalid credentials (URL and/or API key).</p>";
+$contact = array(
+	"email"              => "nsfitzgerald@gmail.com",
+	"first_name"         => "Nathan",
+	"last_name"          => "Fitzgerald",
+	"p[{$list_id}]"      => 1,
+	"status[{$list_id}]" => 1, // "Active" status
+);
+
+$contact_sync = $ac->api("contact/sync", $contact);
+
+if (!(int)$contact_sync->success) {
+	// request failed
+	echo "<p>Syncing contact failed. Error returned: " . $contact_sync->error . "</p>";
 	exit();
 }
-	
-echo "<p>Credentials valid! Proceeding...</p>";
-
-$account = $ac->api("account/view");
-
-echo "<pre>";
-print_r($account);
-echo "</pre>";
-
-echo 'Hello Coaching By Stephanie';
+    
+// successful request
+$contact_id = (int)$contact_sync->subscriber_id;
+echo "<p>Contact synced successfully (ID {$contact_id})!</p>";
