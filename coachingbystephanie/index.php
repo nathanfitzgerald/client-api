71e7305f45ac9a263a4f3fce001af228f257f850
Nathan Fitzgerald <nathan@nathanfitzgerald.com>
2021-06-29 01:00:09 -0700

    include dir

--- coachingbystephanie/index.php
+++ coachingbystephanie/index.php
@@ -2,7 +2,7 @@
 
 define("ACTIVECAMPAIGN_URL", "https://coachingbystephanie.api-us1.com");
 define("ACTIVECAMPAIGN_API_KEY", "fced1b3f5c04f685abbf06da03622f73ddcbd97a1b8ee38bf6905fa37fd67c5c909119e6");
-require_once("./activecampaign-api-php/includes/ActiveCampaign.class.php");
+require_once(__DIR__.'/../activecampaign-api-php/includes/ActiveCampaign.class.php');
 $ac = new ActiveCampaign(ACTIVECAMPAIGN_URL, ACTIVECAMPAIGN_API_KEY);
 
 // Adjust the default cURL timeout
