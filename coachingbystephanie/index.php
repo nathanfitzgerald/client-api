1181670f87737b7870eae395227b4705bf12017b
Nathan Fitzgerald <nathan@nathanfitzgerald.com>
2021-06-29 15:59:57 -0700

    fix header

--- coachingbystephanie/index.php
+++ coachingbystephanie/index.php
@@ -1,6 +1,6 @@
 <?php
 
-header('Content-Type as application/json');
+header('Content-Type: application/json');
 date_default_timezone_set('UTC');
 echo '{ "time": "' . date('Y-m-d H:m:s') . ' UTC" }';
 exit;
