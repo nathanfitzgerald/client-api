de555e00d2270bcff07beb6316209a4f2da53fa4
Nathan Fitzgerald <nathan@nathanfitzgerald.com>
2021-06-29 18:01:11 -0700

    testing

--- coachingbystephanie/abandoned-cart/index.php
+++ coachingbystephanie/abandoned-cart/index.php
@@ -13,21 +13,3 @@ else {
 
 http_response_code(200);
 
-define("ACTIVECAMPAIGN_URL", "https://coachingbystephanie.api-us1.com");
-define("ACTIVECAMPAIGN_API_KEY", "fced1b3f5c04f685abbf06da03622f73ddcbd97a1b8ee38bf6905fa37fd67c5c909119e6");
-require_once(__DIR__.'/../activecampaign-api-php/includes/ActiveCampaign.class.php');
-$ac = new ActiveCampaign(ACTIVECAMPAIGN_URL, ACTIVECAMPAIGN_API_KEY);
-
-// Adjust the default cURL timeout
-$ac->set_curl_timeout(10);
-
-$contact = array(
-	"email"              => "nsfitzgerald@gmail.com",
-	"first_name"         => "Nathan",
-	"last_name"          => "Fitzgerald",
-	"p[{$list_id}]"      => 1,
-	"status[{$list_id}]" => 1, // "Active" status
-);
-
-$contact_sync = $ac->api("contact/sync", $contact);
-
