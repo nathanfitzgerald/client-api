9326d7cd10d213f7a763f81bac2c315cb0548764
Nathan Fitzgerald <nathan@nathanfitzgerald.com>
2021-06-29 20:30:46 -0700

    testing

--- coachingbystephanie/abandoned-cart/index.php
+++ coachingbystephanie/abandoned-cart/index.php
@@ -9,17 +9,7 @@ define("ACTIVECAMPAIGN_API_KEY", "fced1b3f5c04f685abbf06da03622f73ddcbd97a1b8ee3
 require_once(__DIR__.'/../activecampaign-api-php/includes/ActiveCampaign.class.php');
 $ac = new ActiveCampaign(ACTIVECAMPAIGN_URL, ACTIVECAMPAIGN_API_KEY);
 
-if ($json = json_decode(file_get_contents("php://input"), true)) {
-	$data = $json;
-}
-else {
-	$data = $_POST;
-}
 
-// Adjust the default cURL timeout
-$ac->set_curl_timeout(10);
-
-$list_id = 1;
 
 
 
